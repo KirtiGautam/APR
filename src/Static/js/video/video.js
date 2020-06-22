c1f6c6c727932967b46f1f5cbdb2d7fbceba109d
KirtiGautam <gautamkirti8c@gmail.com>
2020-06-22 15:55:54 +0530

    Thumbnails in video section

--- src/Static/js/video/video.js
+++ src/Static/js/video/video.js
@@ -1,16 +1,18 @@
+let thumbarr = [];
+
 const MAS = (url, id) => {
-    $.ajax({
-      type: "POST",
-      headers: { "X-CSRFToken": $('meta[name="csrf-token"]').attr("content") },
-      url: url,
-      data: {
-        id: id,
-      },
-      dataType: "json",
-      success: function (response) {
-        alert(response.message);
-      }, error: function (error) {
-        alert(error.responseText);
-      }
-    });
-  }
+  $.ajax({
+    type: "POST",
+    headers: { "X-CSRFToken": $('meta[name="csrf-token"]').attr("content") },
+    url: url,
+    data: {
+      id: id,
+    },
+    dataType: "json",
+    success: function (response) {
+      alert(response.message);
+    }, error: function (error) {
+      alert(error.responseText);
+    }
+  });
+}
