d7250322c3580a6e451b3e4d54744d141e302cdb
KirtiGautam <gautamkirti8c@gmail.com>
2020-06-25 17:31:30 +0530

    Mail for new user

--- src/APR/settings.py
+++ src/APR/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = '7*_ui$pbr!8^*(+g*h)v#tnuv&9k)2o5+hr@@5+fz^lz=(o-03'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['code.gdy.club', '127.0.0.1', 'localhost', '[2401:4900:45b0:b1f5:9dd7:73e1:8f35:70ce]']
+ALLOWED_HOSTS = ['akshara.ubiqe.in', 'www.akshara.ubiqe.in']
 
 
 # Application definition
@@ -124,12 +124,13 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+MEDIA_URL = '/Media/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "Static"),
 ]
 
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
-MEDIA_URL = '/Media/'
+STATIC_ROOT='/home/getszyrx/APR/public/static'
+MEDIA_ROOT='/home/getszyrx/APR/public/media'
 
 EMAIL_BACKEND = env.str('EMAIL_BACKEND')
 EMAIL_HOST = env.str('EMAIL_HOST')
--- src/APR/urls.py
+++ src/APR/urls.py
@@ -9,4 +9,4 @@ urlpatterns = [
     path('', include('lessons.urls')),
     path('', include('assignments.urls')),
     path('', include('homework.urls')),
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT) + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
