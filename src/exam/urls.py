b895c40d1388c0e31a41548b7830fd4a6273d9b7
KirtiGautam <gautamkirti8c@gmail.com>
2020-08-24 19:20:20 +0530

    Exam settings link

--- src/exam/urls.py
+++ src/exam/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path, re_path
 
-from .views import (index)
+from .views import (index, examSettings)
 
 app_name = 'exam'
 
 urlpatterns = [
     path('exams', index, name='exams'),
+    path('settings/exam-settings', examSettings, name='exam-setiings'),
 ]
--- src/exam/views.py
+++ src/exam/views.py
@@ -1,7 +1,17 @@
 from django.shortcuts import render, redirect
 from django import http
 
+
 def index(request):
     if request.user.is_authenticated:
         return render(request, 'Exam/exams.html')
+    return redirect('accounts:login')
+
+
+def examSettings(request):
+    if request.user.is_authenticated and (request.user.admin or request.user.is_staff):
+        if request.method == "GET":
+            return render(request, 'settings/admin/exams/examsSettings.html')
+        else:
+            pass
     return redirect('accounts:login')
